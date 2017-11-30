836be84866feb898a5c659ca77175ca847a5f3cd
Ryan von Kunes Newton <vonkunesnewton@gmail.com>
2017-11-30 10:16:29 -0800

    Some ISO stuff

--- README.md
+++ README.md
@@ -5,12 +5,13 @@
 * [x] Camera working
 * [ ] Visibility filters
 * [ ] Gesture to adjust visibility
-* [ ] Flash on/off
+* [x] Flash on/off
 
 ###Sprint 2 Goals
 * [ ] NLP Kit, display whats being read
 * [ ] NLP on/off button
 * [ ] Handle rotation
+* [ ] Handle back camera only
 
 ###Sprint 3 Goals 
 * [ ] Finalize design/icons
--- readinthedark/ViewController.swift
+++ readinthedark/ViewController.swift
@@ -13,9 +13,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var previewView: UIView!
     var flashOn = false
-    var captureDevice: AVCaptureDevice?
-    var captureSession: AVCaptureSession?
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var captureDevice: AVCaptureDevice!
+    var captureSession: AVCaptureSession!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,32 +25,46 @@ class ViewController: UIViewController {
     func initializeCamera() {
         do {
             captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
-            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            let input = try AVCaptureDeviceInput(device: captureDevice)
             captureSession = AVCaptureSession()
 
-            captureSession?.addInput(input)
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
-            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-            videoPreviewLayer?.frame = view.layer.bounds
-            previewView.layer.addSublayer(videoPreviewLayer!)
+            captureSession.addInput(input)
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer.frame = view.layer.bounds
+            previewView.layer.addSublayer(videoPreviewLayer)
 
 
             captureSession?.startRunning()
-            NSLog("DONE")
+            
+            // Lock for config. Move this
+            do {
+                try captureDevice.lockForConfiguration()
+                
+            } catch {
+                NSLog("There was some issue with lockForConfiguration")
+            }
+
+            
         } catch {
             print(error)
             return
         }
     }
     
+    
+    @IBAction func onMakeBrighter(_ sender: UIButton) {
+        // Handle min/max iso
+        captureDevice.setExposureModeCustom(duration: AVCaptureDevice.currentExposureDuration, iso: 400, completionHandler: nil)
+        NSLog("\(AVCaptureDevice.currentExposureDuration)")
+        NSLog("\(AVCaptureDevice.currentISO)")
+        NSLog("\(captureDevice.activeFormat.minISO)")
+        NSLog("\(captureDevice.activeFormat.maxISO)")
+    }
+    
     @IBAction func flashButton(_ sender: UIButton) {
         flashOn = !flashOn
-        do {
-            try captureDevice?.lockForConfiguration()
-            captureDevice?.torchMode = flashOn ? .on : .off
-        } catch {
-            NSLog("There was some issue with lockForConfiguration")
-        }
+        captureDevice.torchMode = flashOn ? .on : .off
     }
 
 }

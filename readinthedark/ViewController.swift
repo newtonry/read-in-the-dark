2130acc3ae502ef8018d6ea3bea297ae3bcc09e9
Ryan von Kunes Newton <vonkunesnewton@gmail.com>
2017-11-30 11:50:07 -0800

    Add Vision

--- readinthedark/ViewController.swift
+++ readinthedark/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var previewView: UIView!
     var flashOn = false
+    
+    var requests = [VNRequest]()
+    
+    
     var lastTranslationX: CGFloat = 0
     var captureDevice: AVCaptureDevice!
     var captureSession: AVCaptureSession!
@@ -21,6 +26,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initializeCamera()
+        startTextDetection()
     }
 
     func initializeCamera() {
@@ -28,8 +34,13 @@ class ViewController: UIViewController {
             captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
             let input = try AVCaptureDeviceInput(device: captureDevice)
             captureSession = AVCaptureSession()
+            
+            let output = AVCaptureVideoDataOutput()
+            output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)]
+            output.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default))
 
             captureSession.addInput(input)
+            captureSession.addOutput(output)
             videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
             videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
             videoPreviewLayer.frame = view.layer.bounds
@@ -51,6 +62,25 @@ class ViewController: UIViewController {
             return
         }
     }
+    
+    func startTextDetection() {
+        let textRequest = VNDetectTextRectanglesRequest(completionHandler: self.detectTextHandler)
+        textRequest.reportCharacterBoxes = true
+        self.requests = [textRequest]
+    }
+    
+    func detectTextHandler(request: VNRequest, error: Error?) {
+        guard let observations = request.results else {
+            print("no result")
+            return
+        }
+        
+        let result = observations.map({$0 as? VNTextObservation})
+        
+        NSLog("\(result)")
+    }
+    
+    
     
     func getNewISO(translation: CGPoint, velocity: CGPoint) -> Float {
         let offset = (translation.x - lastTranslationX) * 3 // TODO add something with velocity later
@@ -78,5 +108,29 @@ class ViewController: UIViewController {
         captureDevice.torchMode = flashOn ? .on : .off
     }
 
+}
+
+
+// TODO Review all this
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        var requestOptions:[VNImageOption : Any] = [:]
+        
+        if let camData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics:camData]
+        }
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: 6)!, options: requestOptions)
+        
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
 }
 
